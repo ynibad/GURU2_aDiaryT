f5beb7d45579652ccbdfbcede762592a09d4156d
sasha <seohyun49@swu.ac.kr>
2024-01-29 03:13:52 +0900

    스플래시 화면, 글쓰기 페이지

---
+++ app/src/main/java/com/example/guru2_adiaryt/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.guru2_adiaryt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 3000)
+    }
---
+++ app/src/main/java/com/example/guru2_adiaryt/board/BoardWriteActivity.kt
@@ -0,0 +1,15 @@
+package com.example.guru2_adiaryt.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.guru2_adiaryt.R
+
+class BoardWriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_write)
+
+    }
+}
